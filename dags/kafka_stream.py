10fe54b8d6c9a69001dddeaec35ebde769b6b937
rishabh-jain28 <rishabhj2503@gmail.com>
2024-02-08 12:29:15 +0530

    adding docker file

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -7,12 +7,38 @@ default_args = {
     'start_date':datetime(2024,2,7,10,00)
 }
 
-def stream_data():
+def get_data():
     import json
     import requests
     
     res = requests.get('https://randomuser.me/api/')
-    print(res.json())
+    res = res.json()
+    res = res['results'][0]
+    return res
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+def stream_data():
+    import json
+    res=get_data()
+    res = format_data(res)
+    print(json.dumps(res,indent=3))
     
 # with DAG('user_automation',
 #          default_args=default_args,
