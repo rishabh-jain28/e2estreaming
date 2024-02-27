d0d0c117dc3124d8910b21bfd91a66886f891de0
rishabh-jain28 <rishabhj2503@gmail.com>
2024-02-27 13:07:18 +0530

    added pyspark script

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -46,7 +46,7 @@ def stream_data():
     curr_time = time.time()
 
     while True:
-        if time.time() > curr_time + 60
+        if time.time() > curr_time + 60: #1 minute
             break
         try:
             res = get_data()
